d53d42f0150e191b49be468015dd6b1cd87dac52
Veselin Ivanov <me@veselyn.com>
2024-02-09 20:42:46 +0100

    build(flake): init

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "veselyn.github.io";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils/main";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShell = pkgs.mkShell {};
+    });
+}
